b12b97ac107bdfad47aebc7aca825cdb1da94c97
panmai0tanu <panmai_tanu@cmu.ac.th>
2018-07-03 11:15:35 +0700

    add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'hompages#index'
   resources :hompages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
