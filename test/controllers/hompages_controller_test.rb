5f102d26eefcfb957f5e339ab8bd3136c38ebb97
panmai0tanu <panmai_tanu@cmu.ac.th>
2018-07-03 10:55:43 +0700

    rake test

--- test/controllers/hompages_controller_test.rb
+++ test/controllers/hompages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HompagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get hompages_index_url
+    get hompages_path
     assert_response :success
   end
 
